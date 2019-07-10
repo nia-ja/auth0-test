f57b8062062abd40d03996635d1fd828ea2a9840
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-07-09 22:00:45 -0700

    Create react-app

---
+++ test-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+let state = {};
+
+window.setState = changes => {
+    state = Object.assign({}, state, changes);
+
+    // add props drilling
+    ReactDOM.render(<App {...state} />, document.getElementById('root'));
+}
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
